2997719bf1069657f8471f7d671cb244b5aa4dfb
Pankaj Kumar <pankajkmr036@gmail.com>
2019-07-22 19:46:03 +0530

    Finished all CRUD operations with react and redux

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+
+class GoogleAuth extends Component {
+  componentDidMount() {
+    window.gapi.load("client: auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "435626966380-o81dspuima7it17r9v1jsctjhdt1e67j.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign in with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.renderAuthButton()}</h1>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isSignedIn: state.auth.isSignedIn
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
---
+++ client/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import GoogleAuth from "./GoogleAuth";
+
+function Header() {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamy
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All streams
+        </Link>
+        <GoogleAuth />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
